be52af5ece4f9802e64e4005d3c7f2896173b218
Adam Jarvis <jarvisa@algonquincollege.com>
2025-05-29 11:16:11 -0400

    creating cards dynamically

--- scripts/memory.js
+++ scripts/memory.js
@@ -15,3 +15,39 @@ let score=0, moves=0;
  * 	Scripts added in class
 ********************************/
 
+//use a loop to add multiple cards
+// a for loop has three parts:
+//  set the counter variable (let i=0) and start it at a number
+//  give the loop a condition to keep counting (keep going as long as i is less than 12)
+//  tell the counter to increment each time the loop runs (i++ = add 1 to i)
+for(let i=0; i<12; i++){
+
+    //create the new card (a div element)
+    const card = document.createElement("div");
+
+    //add the class "card" to the new element
+    card.classList.add("card");
+
+    //create the paragraph element
+    const para = document.createElement("p");
+
+    //add the question mark inside the paragraph
+    para.textContent = "?";
+
+    //add the new paragraph to the card
+    card.appendChild(para);
+
+    //add an event listener to the card, when the user clicks
+    //this actually says:
+    // when the user clicks on a card, run the function "flipCard"
+    card.addEventListener("click", flipCard);
+
+    //add the card to the page
+    document.querySelector("main").appendChild(card);
+}
+
+//when the user clicks on the card...
+function flipCard(){
+    
+}
+
